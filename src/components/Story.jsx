4c8f41cb029928498942689477fc7718f98838dc
aminlimbu <aminlimbu@htmail.com>
2023-08-21 01:01:43 +1000

    styles updated for small devices

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -3,7 +3,7 @@ const Story = () => {
         <div className="our-story">
             <h2>Our Story</h2>
             <div className="story">
-                <div className="sotry-image">
+                <div className="story-image">
                     {/* <img
                     src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flive.staticflickr.com%2F2387%2F1655214203_598d2e3acb.jpg&f=1&nofb=1&ipt=fd1988aefaf2390322027952feeb3ee3b4dcf8184da16f5bbfb2cc781b0ffe1e&ipo=images"
                     alt="candle story"
